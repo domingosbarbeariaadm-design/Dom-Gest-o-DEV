53fa3fa922958a622cfd066fc10364944a85b5c2
domingosbarbeariaadm-design <domingosbarbearia.adm@gmail.com>
2026-05-18 05:54:34 -0300

    Substitui placeholder por chave publishable do Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.APP_CONFIG = window.APP_CONFIG || {
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: ''
+window.DOM_GESTAO_CONFIG = {
+  SUPABASE_URL: "https://wlonvksvjmrmsyvamequ.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_2kxxq9p4wlonvksvjmrmsyvamequ_7Yb3kP5rV6dN8tQ1mZ4aC0uH9eL2fG"
 };
